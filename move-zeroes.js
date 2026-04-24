e7cfe42a436e9f821559e30e11cbd75dbab56b21
samarthrjoshi2005-design <samarthrjoshi2005@gmail.com>
2026-04-24 11:34:17 +0530

    Add move zeroes solution

---
+++ move-zeroes.js
@@ -0,0 +1,12 @@
+var moveZeroes = function(nums) {
+    let j = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            let temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            j++;
+        }
+    }
+};
